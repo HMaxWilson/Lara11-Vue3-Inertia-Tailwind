62d1aa73d66d8fdedcf4bdbefa6b439546ca683c
HMaxWilson <h.maxwilson@hotmail.com>
2024-10-04 13:54:03 +0100

    Initial Build

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="flex justify-center">
+        <h1 class="text-3xl font-mono">Welcome to Laravel 11 with InertiaJS, Vue 3 and Tailwind 3</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Welcome'
+    }
+</script>
